dc93bf6f87eab6bc385a1552d9f088f47f568dd5
LogosItAcademy <Tester99037@gmail.com>
2019-01-23 01:33:26 +0200

    commit#6

--- lesson-11/src/main/java/ua/lviv/lgs/dao/impl/BucketDaoImpl.java
+++ lesson-11/src/main/java/ua/lviv/lgs/dao/impl/BucketDaoImpl.java
@@ -14,9 +14,7 @@ import org.apache.log4j.Logger;
 import ua.lviv.lgs.dao.BucketDao;
 import ua.lviv.lgs.domain.Bucket;
 import ua.lviv.lgs.utils.ConnectionUtils;
-
-
-//This class is used for storing Buckets modes to dataBase
+//some comment
 public class BucketDaoImpl implements BucketDao {
 	private static String READ_ALL = "select * from bucket";
 	private static String CREATE = "insert into bucket(`user_id`, `product_id`, `purchase_date`) values (?,?,?)";
