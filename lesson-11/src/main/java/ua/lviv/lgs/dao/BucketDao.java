b4f33a94602a506d39815587a2fb4359de4bd071
LogosItAcademy <Tester99037@gmail.com>
2019-01-23 01:50:52 +0200

    super functionality commit#1

--- lesson-11/src/main/java/ua/lviv/lgs/dao/BucketDao.java
+++ lesson-11/src/main/java/ua/lviv/lgs/dao/BucketDao.java
@@ -3,6 +3,8 @@ package ua.lviv.lgs.dao;
 import ua.lviv.lgs.domain.Bucket;
 import ua.lviv.lgs.shared.AbstractCRUD;
 
+//super functionality #1001
+
 public interface BucketDao extends AbstractCRUD<Bucket>{
 
 }
